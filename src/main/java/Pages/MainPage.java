0d677077c87aae1fb9ec39d82e8e9af777c2bffd
Vladislav <vladislav.ruban@tech-stack.io>
2020-09-17 17:32:27 +0300

    All @FindBy and xpath's updated

--- src/main/java/Pages/MainPage.java
+++ src/main/java/Pages/MainPage.java
@@ -4,17 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 
 public class MainPage extends BasePage{
     public MainPage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(how = How.XPATH, using = "//a[@class='ga_cats_lateral'][@data-tracker-cid='6']")
+    @FindBy(xpath = ".//a[@class='ga_cats_lateral'][@data-tracker-cid='6']")
     private WebElement categoryMobileConnectionButton;
 
-    @FindBy(how = How.XPATH, using = "//a[@class='ga_cats_lateral'][@data-tracker-cid='52']")
+    @FindBy(xpath = ".//a[@class='ga_cats_lateral'][@data-tracker-cid='52']")
     private WebElement categoryMobilePhonesButton;
 
     public void goToCategoryMobilePhones() {
--- src/main/java/Pages/ProductPage.java
+++ src/main/java/Pages/ProductPage.java
@@ -4,14 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 
 public class ProductPage extends BasePage {
     public ProductPage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(how = How.XPATH, using = "//span[contains(@class, 'add-to-wishlist-link')]")
+    @FindBy(xpath = ".//span[contains(@class, 'add-to-wishlist-link')]")
     private WebElement addToWishlistButton;
 
     public void addToWishlist() {
