901100e03d5eb559d5e95490f63fae5f75856703
Vladislav <vladislav.ruban@tech-stack.io>
2020-10-05 18:18:21 +0300

    Added plugin "pretty" to RunBddTests' CucumberOptions

--- src/test/java/RunBddTests.java
+++ src/test/java/RunBddTests.java
@@ -1,6 +1,6 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = {"src/test/resources/features"}, plugin = {"pretty"})
 public class RunBddTests extends AbstractTestNGCucumberTests {
 }
